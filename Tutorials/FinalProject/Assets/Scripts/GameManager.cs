38a310e4ba6547d72a2bab1f54e02fd0c6360503
MihailBancov <mihail.bancov@isa.utm.md>
2024-09-28 18:39:21 +0300

    Final Project. Adding difficulty

--- Tutorials/FinalProject/Assets/Scripts/GameManager.cs
+++ Tutorials/FinalProject/Assets/Scripts/GameManager.cs
@@ -11,16 +11,14 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     private int score;
     private float spawnRate = 1.0f;
     public bool isGameActive;
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -56,5 +54,16 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.SetActive(false);
     }
 }
